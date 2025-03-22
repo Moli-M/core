e198307f0f58753819bfbc5a9dd4eaba743c0e08
Diego <diegomolinero842@gmail.com>
2025-03-22 23:31:22 +0100

    Return to template

--- src/contracts/test.h
+++ src/contracts/test.h
@@ -13,6 +13,22 @@ public:
     struct Burn_input{};
     struct Burn_output{};
 
+    struct SetToken_input
+    {
+        // char name[20];
+        // char symbol[10];
+        uint64 totalSupply;
+    };
+    struct SetToken_output{};
+
+    struct GetToken_input{};
+    struct GetToken_output
+    {
+        // char name[20];
+        // char symbol[10];
+        uint64 totalSupply;
+    };
+
     struct GetStats_input {};
     struct GetStats_output
     {
@@ -23,6 +39,14 @@ public:
 private:
     uint64 numberOfEchoCalls;
     uint64 numberOfBurnCalls;
+
+    struct Token {
+        // char name[20];
+        // char symbol[10];
+        uint64 totalSupply;
+    };
+
+    Token token;
 
     /**
     Send back the invocation amount
@@ -46,6 +70,27 @@ private:
         }
     _
 
+    PUBLIC_PROCEDURE(SetToken)        
+        Token tempToken;
+        // Copiar cada carácter de input.name a tempToken.name
+        // for (int i = 0; i < 20; i++) {
+        //     tempToken.name[i] = input.name[i];
+        // }
+        // // Copiar cada carácter de input.symbol a tempToken.symbol
+        // for (int i = 0; i < 10; i++) {
+        //     tempToken.symbol[i] = input.symbol[i];
+        // }
+        tempToken.totalSupply = input.totalSupply;
+
+        state.token = tempToken;
+    _
+
+    PUBLIC_FUNCTION(GetToken)
+        // output.name = state.token.name;
+        // output.symbol = state.token.symbol;
+        output.totalSupply = state.token.totalSupply;
+    _
+
     PUBLIC_FUNCTION(GetStats)
         output.numberOfBurnCalls = state.numberOfBurnCalls;
         output.numberOfEchoCalls = state.numberOfEchoCalls;
@@ -55,12 +100,15 @@ private:
 
         REGISTER_USER_PROCEDURE(Echo, 1);
         REGISTER_USER_PROCEDURE(Burn, 2);
+        REGISTER_USER_PROCEDURE(SetToken, 3);
 
         REGISTER_USER_FUNCTION(GetStats, 1);
+        REGISTER_USER_FUNCTION(GetToken, 2);
     _
 
     INITIALIZE
         state.numberOfEchoCalls = 0;
         state.numberOfBurnCalls = 0;
+        state.token.totalSupply = 25;
     _
 };
